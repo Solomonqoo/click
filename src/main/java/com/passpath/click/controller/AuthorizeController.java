9b07f93248697d7d86a163ea7018146bc9d5db36
Solomonqoo <solomonqoo@gmail.com>
2020-04-28 09:34:52 +0800

    init repo

---
+++ src/main/java/com/passpath/click/controller/AuthorizeController.java
@@ -0,0 +1,64 @@
+package com.passpath.click.controller;
+
+import com.passpath.click.dto.AccessTokenDTO;
+import com.passpath.click.dto.GithubUser;
+import com.passpath.click.mapper.UserMapper;
+import com.passpath.click.model.User;
+import com.passpath.click.provider.GithubProvider;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Controller;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+
+import javax.servlet.http.HttpServletRequest;
+import java.util.UUID;
+
+@Controller
+public class AuthorizeController {
+
+    @Autowired
+    private GithubProvider githubProvider;
+
+
+    @Value("${github.client_id}")
+    private String clientId;
+    @Value("${github.client_secret}")
+    private String clientSecret;
+    @Value("${github.redirect_uri}")
+    private String redirectUri;
+
+    @Autowired
+    private UserMapper userMapper;
+
+    @GetMapping("/callback")
+    public String callback(@RequestParam(name="code") String code,
+                           @RequestParam(name="state") String state,
+                           HttpServletRequest request) {
+        AccessTokenDTO accessTokenDTO = new AccessTokenDTO();
+        accessTokenDTO.setCode(code);
+        accessTokenDTO.setRedirect_uri(redirectUri);
+        accessTokenDTO.setState(state);
+        accessTokenDTO.setClient_id(clientId);
+        accessTokenDTO.setClient_secret(clientSecret);
+
+        String accessToken = githubProvider.getAccessToken(accessTokenDTO);
+        GithubUser githubUser = githubProvider.getUser(accessToken);
+        if (githubUser!=null) {
+            User user = new User();
+            user.setToken(UUID.randomUUID().toString());
+            user.setName(githubUser.getName());
+            user.setAccountId(String.valueOf(githubUser.getId()));
+            user.setGmtCreate(System.currentTimeMillis());
+            user.setGmtModified(user.getGmtCreate()); //這句不懂！
+            userMapper.insert(user);
+
+            //登錄成功，寫cookies、session
+            request.getSession().setAttribute("user", user);
+            return "redirect:/";
+        }else {
+            //登錄失敗，重新登錄
+            return "redirect:/";
+        }
+    }
+}
---
+++ src/main/java/com/passpath/click/dto/GithubUser.java
@@ -0,0 +1,31 @@
+package com.passpath.click.dto;
+
+public class GithubUser {
+    private String name;
+    private Long id;   //怕將來github用戶爆增，id長度不夠用
+    private String email;
+
+    public String getName() {
+        return name;
+    }
+
+    public void setName(String name) {
+        this.name = name;
+    }
+
+    public Long getId() {
+        return id;
+    }
+
+    public void setId(Long id) {
+        this.id = id;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+}
